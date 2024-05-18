1023e2f01226e11187ca78d7c1a9f328022cf388
Filip46820 <144820919+Filip46820@users.noreply.github.com>
2024-05-18 21:26:09 +0200

    Save/Load function

--- script.js
+++ script.js
@@ -52,6 +52,7 @@ function Userclicking(){
     document.querySelector('.CurrentBalance').innerHTML=ClickerData.balance;
 }
 
+//  Funkce pro zakoupeni vylepšení
 function BuyUpgrade(UpgradeIndex){
     const Upgrade = ClickerData.upgrades[UpgradeIndex];
     const UpgradeNextCost = Math.ceil(Upgrade.costBase*(Math.pow(Upgrade.rateGrowth, Upgrade.owned)));
@@ -72,6 +73,44 @@ setInterval(function(){
         document.querySelector('.CurrentBalance').innerHTML=ClickerData.balance;
     })
 },1000);
+
+function save(){
+    localStorage.clear();
+    ClickerData.upgrades.map((upgrades) => {
+        const obj = JSON.stringify({
+            SOwned: upgrades.owned,
+            SCostNext: upgrades.costNext,
+        });
+        console.log(obj); 
+        localStorage.setItem(upgrades.name, obj);
+    });
+    localStorage.setItem('balance', JSON.stringify(ClickerData.balance));
+    console.log(localStorage);
+}
+
+function load(){
+    if(localStorage.getItem('balance')!==null){
+        ClickerData.balance = parseFloat(localStorage.getItem('balance'));
+        document.querySelector('.CurrentBalance').innerHTML=ClickerData.balance;
+    }else{
+        alert("Zadny save nenalezen");
+    }
+    ClickerData.upgrades.map((upgrades) => {
+        if(localStorage.getItem(upgrades.name)!==null){
+            const SavedData = JSON.parse(localStorage.getItem(upgrades.name));
+            upgrades.owned = SavedData.SOwned;
+            upgrades.costNext = SavedData.SCostNext;
+            document.querySelector(`.${upgrades.name}-owned`).innerHTML = upgrades.owned;
+            document.querySelector(`.${upgrades.name}-price`).innerHTML = upgrades.costNext;
+        }
+    });
+}
+
+// Add format for numbers? 1000=1k
+// Prestige?
+// Load?
+// Reset progress?
+// Talents/Perks?
 
 /*  Funkce pro nastaveni peněz přes konzoli
     Pro použití - GameEarn(cislo) do konzole    */
