b52e273aac33b278b1a7aa5aa285047493e2def9
Filip46820 <144820919+Filip46820@users.noreply.github.com>
2024-05-18 13:45:18 +0200

    Upgrades functionability, site scaling
    Added script for upgrades to work and for site to change position of elements when screen resolution is too small to fit upgrades

--- script.js
+++ script.js
@@ -1,53 +1,82 @@
-//Zakladni data pro vylepšení a peníze
+//  Zakladni data pro vylepšení a peníze
 const ClickerData = {
     balance: 0,
     upgrades: [
         {
-            name: "Click",
+            name: "click",
             owned: 0,
             costBase: 10,
-            rateGrowth: 1.04,
+            rateGrowth: 1.06,
             costNext: 10,
-            PerSecond: 0,
+            perSecond: 0,
         },
         {
-            name: "Fishing",
+            name: "fishing",
             owned: 0,
             costBase: 100,
             rateGrowth: 1.06,
             costNext: 100,
-            PerSecond: 2,
+            perSecond: 2,
         },
         {
-            name: "Mining",
+            name: "mining",
             owned: 0,
             costBase: 400,
             rateGrowth: 1.08,
             costNext: 400,
-            PerSecond: 5,
+            perSecond: 5,
         },
         {
-            name: "Gardening",
+            name: "gardening",
             owned: 0,
             costBase: 1000,
             rateGrowth: 1.085,
             costNext: 1000,
-            PerSecond: 15,
+            perSecond: 15,
         },
         {
-            name: "Engineering",
+            name: "engineering",
             owned: 0,
             costBase: 7500,
             rateGrowth: 1.09,
             costNext: 7500,
-            PerSecond: 50,
+            perSecond: 50,
         },
     ],
 };
 
-//Zakladni funkce pro přidani peněz za kliknutí
+//  Zakladni funkce pro přidani peněz za kliknutí
 function Userclicking(){
     const ClickUpgradesOwned = ClickerData.upgrades[0].owned;
     ClickerData.balance += 1 + ClickUpgradesOwned;
     document.querySelector('.CurrentBalance').innerHTML=ClickerData.balance;
+}
+
+function BuyUpgrade(UpgradeIndex){
+    const Upgrade = ClickerData.upgrades[UpgradeIndex];
+    const UpgradeNextCost = Math.ceil(Upgrade.costBase*(Math.pow(Upgrade.rateGrowth, Upgrade.owned)));
+    if(ClickerData.balance >= UpgradeNextCost){
+        ClickerData.balance -= UpgradeNextCost;
+        Upgrade.owned++;
+        Upgrade.costNext=Math.ceil(Upgrade.costBase*(Math.pow(Upgrade.rateGrowth, Upgrade.owned)));
+        document.querySelector('.CurrentBalance').innerHTML=ClickerData.balance;
+        document.querySelector(`.${Upgrade.name}-owned`).innerHTML = Upgrade.owned;
+        document.querySelector(`.${Upgrade.name}-price`).innerHTML = Upgrade.costNext;
+    }else{
+        alert("Not enough money to buy this upgrade");
+    }
+} 
+setInterval(function(){
+    ClickerData.upgrades.forEach(Upgrade =>{
+        ClickerData.balance += Upgrade.perSecond*Upgrade.owned;
+        document.querySelector('.CurrentBalance').innerHTML=ClickerData.balance;
+    })
+},1000);
+
+/*  Funkce pro nastaveni peněz přes konzoli
+    Pro použití - GameEarn(cislo) do konzole    */
+function GameEarn(Number){
+    ClickerData.balance += Number;
+    document.querySelector('.CurrentBalance').innerHTML=ClickerData.balance;
+    return ClickerData.balance; //Bez toho - Undefined
 }
